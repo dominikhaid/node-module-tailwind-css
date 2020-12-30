96ad37c8a270cc1b0297b51ca028309f38c15439
dominikhaid <info@dominikhaid.de>
2020-12-30 16:12:15 +0100

    ne preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,81 +1,44 @@
-// Example `tailwind.config.js` file
-const colors = require('tailwindcss/colors')
+// Example `tailwind.config.js` filecorePlugins  from ('./tailwind/core');
+const spacing = require('./tailwind/spacing');
+const colors = require('./tailwind/colors');
+const fontSize = require('./tailwind/fontsize');
+const screens = require('./tailwind/screens');
+const width = require('./tailwind/width');
+const height = require('./tailwind/height');
+const core = require('./tailwind/core');
+const shadow = require('./tailwind/shadow');
 
 module.exports = {
-  //  purge: ['./pages/**/*.js', './components/**/*.js'],
-   darkMode: false, // or 'media' or 'class'
-  theme: {
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
+  corePlugins: core,
+  variants: {
+    extend: {
+      ringColor: ['hover'],
+      ringWidth: ['hover'],
+      borderColor: ['active', 'hover'],
+      borderWidth: ['active', 'hover'],
+      opacity: ['disabled'],
     },
   },
-}
+  theme: {
+    spacing: spacing,
+    colors: colors,
+    screens: screens,
+    minWidth: width,
+    maxWidth: width,
+    width: width,
+    height: height,
+    minHeight: height,
+    maxHeight: height,
+    fontFamily: {
+      icon: ['fontawesome'],
+      body: ['Roboto', 'sans-serif'],
+    },
+    boxShadow: shadow,
+    fontSize: fontSize,
+    container: {padding: spacing},
+  },
 
-
-
-// module.exports = {
-//   purge: ['./pages/**/*.js', './components/**/*.js'],
-//   darkMode: false, // or 'media' or 'class'
-//  theme: {
-//    screens: {
-//      'sm': '640px',
-//      'md': '768px',
-//      'lg': '1024px',
-//      'xl': '1280px',
-//      '2xl': '1536px',
-//    },
-//    colors: {
-//      gray: colors.coolGray,
-//      gray: {
-//        darkest: '#1f2d3d',
-//        dark: '#3c4858',
-//        DEFAULT: '#c0ccda',
-//        light: '#e0e6ed',
-//        lightest: '#f9fafc',
-//      },
-//      indigo: {
-//        light: '#b3bcf5',
-//        DEFAULT: '#5c6ac4',
-//        dark: '#202e78',
-//      },      
-//      primary: 'var(--color-primary)',
-//      secondary: 'var(--color-secondary)',
-//    },
-//    fontFamily: {
-//      sans: ['Graphik', 'sans-serif'],
-//      serif: ['Merriweather', 'serif'],
-//    },
-//    spacing: {
-//      sm: '8px',
-//      md: '12px',
-//      lg: '16px',
-//      xl: '24px',
-//    },
-//    borderRadius: {
-//      'none': '0',
-//      'sm': '.125rem',
-//      DEFAULT: '.25rem',
-//      'lg': '.5rem',
-//      'full': '9999px',
-//    },
-//    extend: {
-//      spacing: {
-//        '128': '32rem',
-//        '144': '36rem',
-//      },
-//      borderRadius: {
-//        '4xl': '2rem',
-//      }
-//    }
-//  },
-//  variants: {
-//    extend: {
-//      borderColor: ['focus-visible'],
-//      opacity: ['disabled'],
-//    }
-//  }
-// }
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
+};
---
+++ tailwind/screens.js
@@ -0,0 +1,7 @@
+module.exports = {
+  '2xl': {min: '2500px', max: '8000px'},
+  xl: {min: '1381px', max: '2499px'},
+  lg: {min: '769px', max: '1380px'},
+  md: {min: '641px', max: '768px'},
+  sm: {min: '300px', max: '640px'},
+};
